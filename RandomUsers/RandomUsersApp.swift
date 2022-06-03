7fdbaaadf6ad3fc953da48028e3ba854537faf4b
Richard Garrison <anm8tr@users.noreply.github.com>
2022-06-03 14:57:30 -0700

    Initial Commit

---
+++ RandomUsers/RandomUsersApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomUsersApp.swift
+//  RandomUsers
+//
+//  Created by Richard Garrison on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct RandomUsersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
